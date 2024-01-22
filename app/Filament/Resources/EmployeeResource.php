4b7e2b1f0246a0ce06e4958c5b38b474a774c798
Salem Moqa <moqasalem@gmail.com>
2024-01-22 11:49:10 +0300

    live change in create employee page

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -4,7 +4,10 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\EmployeeResource\Pages;
 use App\Filament\Resources\EmployeeResource\RelationManagers;
+use App\Models\City;
+use App\Models\Country;
 use App\Models\Employee;
+use App\Models\State;
 use Filament\Forms;
 use Filament\Forms\Components\Card;
 use Filament\Forms\Components\DatePicker;
@@ -30,22 +33,41 @@ class EmployeeResource extends Resource
         return $form
             ->schema([
                 Card::make()
-                ->schema([
-                    Select::make('country_id')
-                    ->relationship('country', 'name')->required(),
-                    Select::make('state_id')
-                    ->relationship('state', 'name')->required(),
-                    Select::make('city_id')
-                    ->relationship('city', 'name')->required(),
-                    Select::make('department_id')
-                    ->relationship('department', 'name')->required(),
-                TextInput::make('first_name')->required(),
-                TextInput::make('last_name')->required(),
-                TextInput::make('address')->required(),
-                TextInput::make('zip_code')->required(),
-                DatePicker::make('birth_date')->required(),
-                DatePicker::make('date_hired')->required(),
-                ])
+                    ->schema([
+                        Select::make('country_id')
+                            ->label('Country')
+                            ->options(Country::all()->pluck('name', 'id')->toArray())
+                            ->reactive()
+                            ->afterStateUpdated(fn(callable $set) => $set('state_id', null)),
+                        Select::make('state_id')
+                            ->label('State')
+                            ->options(function (callable $get) {
+                                $country = Country::find($get('country_id'));
+                                if (!$country)
+                                    return State::all()->pluck('name', 'id');
+                                return $country->states->pluck('name', 'id');
+                            })
+                            ->reactive()
+                            ->afterStateUpdated(fn(callable $set) => $set('city_id', null)),
+                        Select::make('city_id')
+                            ->label('City')
+                            ->options(function (callable $get) {
+                                $state = State::find($get('state_id'));
+                                if (!$state)
+                                    return City::all()->pluck('name', 'id');
+                                return $state->cities->pluck('name', 'id');
+                            })
+                            ->reactive()
+                            ->afterStateUpdated(fn(callable $set) => $set('city_id', null)),
+                        Select::make('department_id')
+                            ->relationship('department', 'name')->required(),
+                        TextInput::make('first_name')->required(),
+                        TextInput::make('last_name')->required(),
+                        TextInput::make('address')->required(),
+                        TextInput::make('zip_code')->required(),
+                        DatePicker::make('birth_date')->required(),
+                        DatePicker::make('date_hired')->required(),
+                    ])
             ]);
     }
 
@@ -61,7 +83,7 @@ class EmployeeResource extends Resource
                 TextColumn::make('created_at')->dateTime()
             ])
             ->filters([
-                SelectFilter::make('department')->relationship('department','name')
+                SelectFilter::make('department')->relationship('department', 'name')
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
